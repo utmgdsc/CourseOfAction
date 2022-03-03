31cbdb0a95f1264ee56d6f603e5917cde58b3207
Mitesh Ghimire <mitesh.ghimire@mail.utoronto.ca>
2022-03-03 00:12:49 -0500

    Cleaner Add course ui

--- frontend/src/pages/AddCourse.tsx
+++ frontend/src/pages/AddCourse.tsx
@@ -5,12 +5,14 @@ import {
   TextField,
   Slider,
   MenuItem,
+  Button,
+  Grid,
 } from "@mui/material";
 import React, { useState } from "react";
 import FileUploader from "../components/Syllabus";
 import { CourseInterface, Assessment } from "../store/courses";
 import Assessments from "../components/Assessments";
-
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 function AddCourse() {
   const [assessments, setAssessments] = useState<Assessment[]>([]);
   return (
@@ -26,38 +28,56 @@ function AddCourse() {
         Input Manually
       </Typography>
       <Box>
-        <form
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            maxWidth: "600px",
-          }}
-        >
-          <Typography variant="h6">Course Code</Typography>
-          <TextField id="outlined-basic" variant="outlined" />
-          <Typography variant="h6">Course Name</Typography>
-          <TextField id="outlined-basic" variant="outlined" />
-          <Typography variant="h6">Credit</Typography>
-          <TextField id="outlined-basic" select type="number">
-            <MenuItem value={0.5}>0.5</MenuItem>
-            <MenuItem value={1}>1</MenuItem>
-          </TextField>
-          <Typography variant="h6">Expected Grade</Typography>
-          <TextField id="outlined-basic" variant="outlined" type="number" />
-          <Typography variant="h6">Familiarity</Typography>
-          <Slider
-            aria-label="Small steps"
-            defaultValue={5}
-            step={1}
-            marks
-            min={0}
-            max={10}
-            valueLabelDisplay="auto"
-          />
-          <Typography variant="h6">Offering</Typography>
-          <TextField id="outlined-basic" variant="outlined" />
-        </form>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 3, md: 6 }}>
+          <Grid container item xs={6} direction="column">
+            <Typography variant="h6">Course Code</Typography>
+            <TextField id="outlined-basic" variant="outlined" />
+            <Typography variant="h6">Offering</Typography>
+            <TextField id="outlined-basic" variant="outlined" />
+            <Typography variant="h6">Familiarity</Typography>
+            <Box mt={1.5}>
+              <Slider
+                aria-label="Small steps"
+                defaultValue={5}
+                step={1}
+                marks
+                min={0}
+                max={10}
+                valueLabelDisplay="auto"
+              />
+            </Box>
+          </Grid>
+          <Grid container item xs={6} direction="column">
+            <Typography variant="h6">Course Name</Typography>
+            <TextField id="outlined-basic" variant="outlined" />
+            <Typography variant="h6">Credit</Typography>
+            <TextField
+              id="outlined-basic"
+              select
+              type="number"
+              defaultValue={0.5}
+            >
+              <MenuItem value={0.5}>0.5</MenuItem>
+              <MenuItem value={1}>1</MenuItem>
+            </TextField>
+            <Typography variant="h6">Expected Grade</Typography>
+            <TextField id="outlined-basic" variant="outlined" type="number" />
+          </Grid>
+        </Grid>
+        {/* </Box> */}
         <Assessments tableData={assessments} setTableData={setAssessments} />
+        <Box textAlign="center">
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            sx={{ color: "white" }}
+            onClick={() => console.log(1)}
+            endIcon={<CheckCircleIcon />}
+          >
+            Submit
+          </Button>
+        </Box>
       </Box>
     </Container>
   );
