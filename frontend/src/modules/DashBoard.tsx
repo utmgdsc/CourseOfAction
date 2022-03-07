167746d930f08a9580dcee58d9d9980fb21999c7
Mitesh Ghimire <mitesh.ghimire@mail.utoronto.ca>
2022-03-06 23:50:56 -0500

    More updates to Dashboard

--- frontend/src/modules/DashBoard.tsx
+++ frontend/src/modules/DashBoard.tsx
@@ -1,4 +1,4 @@
-import { Container, Button, Typography, Box } from "@mui/material";
+import { Container, Button, Typography, Box, Grid } from "@mui/material";
 import { CourseInterface } from "../store/courses";
 import {
   BarChart,
@@ -60,104 +60,113 @@ function Dashboard({ courses }: propTypes) {
 
   return (
     <Container>
-      <Typography variant="h1" color={COLORS[1]}>
+      <Typography variant="h3" color={COLORS[1]}>
         Dashboard
       </Typography>
       <br />
       <Typography variant="h6">Current Grade Breakdown</Typography>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={6}>
+          <Typography variant="h6">CGPA data here</Typography>
+        </Grid>
+        <Grid item md={3}>
+          <PieChart width={250} height={250}>
+            <Pie
+              data={data}
+              cx="50%"
+              cy="50%"
+              // innerRadius={40}
+              outerRadius={90}
+              dataKey="value"
+            >
+              {data.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                ></Cell>
+              ))}
+            </Pie>
+            <Tooltip content={<CustomTooltip />} />
+          </PieChart>
+        </Grid>
+        <Grid item xs={6}>
+          <Box component="span" sx={{ p: 2 }}>
+            <BarChart
+              width={500}
+              height={200}
+              data={courses}
+              // margin={{
+              //   top: 5,
+              //   right: 30,
+              //   left: 20,
+              //   bottom: 5,
+              // }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey={"code"} />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey={"currMark"} fill={COLORS[0]} />
+              <Bar dataKey={"expectedMark"} fill={COLORS[1]} />
+            </BarChart>
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <Container
+            style={{
+              backgroundColor: "#595959",
+              borderRadius: "18px",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+
+                // justifyContent: "space-between",
+                // borderRadius: "10px",
+                // position: "absolute",
+                // bottom: "0",
+                // right: "0",
+              }}
+            >
+              <Typography>Next Deadline</Typography>
+              <Button
+                variant="contained"
+                color="primary"
+                size="small"
+                // sx={{ }}
+                // onClick={handleSubmit}
+                // endIcon={<CheckCircleIcon />}
+                // disabled={canSubmit()}
+              >
+                More
+              </Button>
+            </Box>
+            <Typography>CSCI1030 - Deadline</Typography>
+            <Typography>CSCI1030 - Deadline</Typography>
+            <Typography>CSCI1030 - Deadline</Typography>
+          </Container>
+        </Grid>
+        <Grid>
+          <Container
+            style={{
+              // display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Typography> What to work on ? </Typography>
+            <Typography> GOOGLE CALANDER</Typography>
+          </Container>
+        </Grid>
+      </Grid>
       <Container
         style={{
           display: "flex",
           alignItems: "center",
         }}
-      >
-        <BarChart
-          width={500}
-          height={300}
-          data={courses}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey={"code"} />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey={"currMark"} fill={COLORS[0]} />
-          <Bar dataKey={"expectedMark"} fill={COLORS[1]} />
-        </BarChart>
-        <PieChart
-          width={450}
-          height={450}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <Pie
-            data={data}
-            cx="50%"
-            cy="50%"
-            // innerRadius={40}
-            outerRadius={90}
-            dataKey="value"
-          >
-            {data.map((entry, index) => (
-              <Cell
-                key={`cell-${index}`}
-                fill={COLORS[index % COLORS.length]}
-              ></Cell>
-            ))}
-          </Pie>
-          <Tooltip content={<CustomTooltip />} />
-        </PieChart>
-      </Container>
-
-      <Container
-        style={{
-          position: "fixed",
-          bottom: "100px",
-          right: "50px",
-          left: "200px",
-          backgroundColor: "#595959",
-          width: "300px",
-          height: "150px",
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-          }}
-        >
-          <Typography>Next Deadline</Typography>
-          <Button
-            variant="contained"
-            color="primary"
-            size="small"
-            // sx={{ color: "white" }}
-            // onClick={handleSubmit}
-            // endIcon={<CheckCircleIcon />}
-            // disabled={canSubmit()}
-          >
-            More
-          </Button>
-        </Box>
-        <Typography>CSCI1030 - Deadline</Typography>
-        <Typography>CSCI1030 - Deadline</Typography>
-        <Typography>CSCI1030 - Deadline</Typography>
-      </Container>
-      <Container>
-        <Typography> What to work on ? </Typography>
-        <Typography> GOOGLE CALANDER</Typography>
-      </Container>
+      ></Container>
     </Container>
   );
 }
