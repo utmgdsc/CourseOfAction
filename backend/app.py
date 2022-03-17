9eaa58bed6b9949a964383a56a31be3c0a052491
Mitesh Ghimire <mitesh.ghimire@mail.utoronto.ca>
2022-03-17 00:13:17 -0400

    Added the server configured flask app

--- backend/app.py
+++ backend/app.py
@@ -1,47 +1,69 @@
 import datetime
 import json
-import os
-import uuid
-import parser
 import pyrebase
-from flask import Flask, jsonify, make_response, request
+import hashlib
+from flask import Flask, jsonify, make_response, request, abort, send_from_directory
 from flask_cors import CORS, cross_origin
-
-app = Flask(__name__)
+import os
+app = Flask(__name__, static_folder='build')
 cors = CORS(app)
+
 # Connect to Firebase Realtime DB
 firebase = pyrebase.initialize_app(json.load(open('secrets.json')))
 auth = firebase.auth()
 # Authenticate Firebase tables
 db = firebase.database()
 
+def get_user(utorid):
+    user = hashlib.sha256(utorid.encode("utf-8")).hexdigest()
+    # check existing users
+    user_exists = db.child("users").child(user).get()
+    if not user_exists.val():
+        # register user in firebase
+        try:
+            db.child("users").child(user).set({"name": request.headers["Http-Cn"], "email": request.headers["Http-Mail"], "courses": []})
+        except:
+            abort(401, description="Unable to create user") # redirect page since app is not accessible
+    return user
 
-def bad_request(mess: str):
-    return make_response(jsonify(message=mess), 400)
+@app.route("/coa/")
+def index():
+    # format_headers = lambda d: '\n'.join(k + ": " +v for k, v in d.items())
+    # data = jsonify(data=(request.method, request.url, "\n\n"+format_headers(request.headers)))
+    user = get_user(request.headers["Utorid"])
+    # redirect user to the app 
+    response = make_response()
+    response.headers['location'] = "/coa/app/" 
+    return response, 302
 
+@app.route('/coa/app/', defaults={'path': ''})
+@app.route("/coa/app/<path:path>")
+def send_app(path):
+    """
+    Serve static files for the frontend app
+    """
+    user = get_user(request.headers["Utorid"])
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
-@app.route("/")
-def hello():
-    return "Hello, welcome to api endpoint for CourseOfAction!"
-
-
-@app.route('/api/get_courses', methods=["GET", "POST"])
+@app.route('/coa/api/get-courses', methods=["GET", "POST"])
 def get_courses():
     """
     Function to retrieve all courses for the student
     """
-    user = "UuT5Mb7uJKO8N6mTTv9LuyCexgl1"
+    user = get_user(request.headers["Utorid"])
     return make_response(jsonify(db.child('users').child(user).child("courses").get().val()), 200)
 
-
-@app.route('/api/add_course', methods=["POST"])
+@app.route('/coa/api/add-course', methods=["POST"])
 @cross_origin()
 def add_course():
     """
     Function to create a new course and add it for that specific user 
     """
-    user = "UuT5Mb7uJKO8N6mTTv9LuyCexgl1"
-    if not (request.json) or not(request.json.get('code', None) and request.json.get('name', None)):
+    user = get_user(request.headers["Utorid"])
+    if not (request.json) or not(request.json.get('code', None)):
         return make_response(jsonify(message='Error missing required course information'), 400)
 
     #make sure the course code is unique
@@ -58,7 +80,6 @@ def add_course():
         return jsonify(messge="success")
     except:
         return make_response(jsonify(message='Error creating course'), 401)
-
 
 def parse_assessments(assessments):
     """ This helper function parses the assessment received from frontend to firebase acceptable format
@@ -77,35 +98,16 @@ def parse_assessments(assessments):
         parsed[assessment.get(name, f"assessment{i}").replace(".", "").strip(',?!')] = cur
     return parsed
 
-
 def generate_reminders(familiarity, assessments):
     """TODO: create this function to handle add and update course api
     Use the familiarity scale to set reminder dates for each assessment
     """
     return assessments
 
-
-@app.route('/api/parse-assessments', methods=["POST"])
-def parse_assessment():
-    # get and save the file
-    if 'file' not in request.files:
-        return bad_request("Bad Request")
-    file = request.files['file']
-    if file.filename == '':
-        return bad_request("File not attached")
-    # create unique name for the file
-    filename = uuid.uuid4()
-    file_path = '/tmp' + os.sep + str(filename)
-    # save file
-    file.save(file_path)
-    # parse assessments
-    parsed_assessments = parser.extract_info(file_path)
-    os.remove(file_path)
-    # check if parsed correctly
-    if isinstance(parsed_assessments, int):
-        return bad_request("The syllabus format is not supported. Please enter your assessments manually.")
-    return { "assessments": parsed_assessments}
+@app.errorhandler(500)
+def app_error(e):
+    return make_response("Application error! Please try again later!"), 500
 
 
 if __name__ == "__main__":
-    app.run() #debug=True for hot reload
+    app.run(debug=True, port=8989)# for hot reload
